7f1239ae9c85324d140e6ff7021eb40bff2d6b8b
Nathan Walston <walston@users.noreply.github.com>
2023-02-02 15:53:16 -0800

    Adds in my tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,29 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from './assets/vite.svg';
-import './App.css';
+import { DndContext, useDraggable } from "@dnd-kit/core";
+import {} from "@jsonforms/vanilla-renderers";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const controls = [
+    "Integer",
+    "Artifact",
+    "Vertical Layout",
+    "Horizontal Layout",
+  ];
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <DndContext>
+        <div className="bucket">
+          {controls.map((name) => {
+            const { setNodeRef } = useDraggable({ id: name });
+            return (
+              <div ref={setNodeRef}>
+                <code>{name}</code>
+              </div>
+            );
+          })}
+        </div>
+        <div className="form"></div>
+      </DndContext>
     </div>
   );
 }
